b1452fa51957776e91e496b8a849d8d5f4ed43c7
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2018-07-27 11:35:25 -0300

    Add Iugu.Plan.show_by_identifier/1

--- lib/resources/plan.ex
+++ lib/resources/plan.ex
@@ -14,4 +14,16 @@ defmodule Iugu.Plan do
       :prices,
       :features
     ]
+
+  @spec show_by_identifier(String.t()) :: Iugu.Request.get_response()
+  @doc """
+  Returns a Iugu's Plan by identifier
+
+  ## Parameters
+    - identifier: Iugu Plan identifier - [Iugu Reference](https://dev.iugu.com/v1.0/reference#testinput-6)
+  """
+  def show_by_identifier(identifier) do
+    %Iugu.Request{path: "plans/identifier/#{identifier}"}
+    |> Iugu.Request.get(__MODULE__, :single)
+  end
 end
