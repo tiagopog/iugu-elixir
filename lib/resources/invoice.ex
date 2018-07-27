e3d0698342d6fa1fe012146de98e5cd82c488991
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2018-07-27 10:33:48 -0300

    Add missing methods of Invoice

--- lib/resources/invoice.ex
+++ lib/resources/invoice.ex
@@ -3,8 +3,10 @@ defmodule Iugu.Invoice do
   Iugu's invoice resource
   """
 
+  @resource "invoices"
+
   use Iugu.Resource,
-    name: "invoices",
+    name: @resource,
     actions: [:list, :show, :create],
     fields: [
       :active,
@@ -30,4 +32,76 @@ defmodule Iugu.Invoice do
       :subitems,
       :suspended
     ]
+
+  @spec capture(String.t()) :: Iugu.Request.post_response()
+  @doc """
+  Capture invoice - [Iugu Reference](https://dev.iugu.com/v1.0/reference#capturar-fatura)
+
+  ## Parameters
+
+    - id: Iugu invoice ID
+  """
+  def capture(id) do
+    %Iugu.Request{path: "#{@resource}/#{id}/capture"}
+    |> Iugu.Request.post(__MODULE__)
+  end
+
+  @spec refund(String.t()) :: Iugu.Request.post_response()
+  @doc """
+  Refund invoice - [Iugu Reference](https://dev.iugu.com/v1.0/reference#reembolsar-fatura)
+
+  ## Parameters
+
+    - id: Iugu invoice ID
+  """
+  def refund(id) do
+    %Iugu.Request{path: "#{@resource}/#{id}/refund"}
+    |> Iugu.Request.post(__MODULE__)
+  end
+
+  @spec cancel(String.t()) :: Iugu.Request.put_response()
+  @doc """
+  Cancel a invoice - [Iugu Reference](https://dev.iugu.com/v1.0/reference#cancelar)
+
+  ## Parameters
+
+    - id: Iugu invoice ID
+  """
+  def cancel(id) do
+    %Iugu.Request{path: "#{@resource}/#{id}/cancel"}
+    |> Iugu.Request.put(__MODULE__)
+  end
+
+  @spec duplicate(map, String.t()) :: Iugu.Request.post_response()
+  @doc """
+  Duplicate invoice - [Iugu Reference](https://dev.iugu.com/v1.0/reference#gerar-segunda-via)
+
+  ## Parameters
+
+    - data: Map with `:due_date` (required) and another parameters
+    - id: Iugu invoice ID
+  """
+  def duplicate(%{} = data, id) do
+    case Poison.encode(data) do
+      {:ok, json} ->
+        %Iugu.Request{body: json, path: "#{@resource}/#{id}/duplicate"}
+        |> Iugu.Request.post(__MODULE__)
+
+      {status, result} ->
+        {status, result}
+    end
+  end
+
+  @spec send_email(String.t()) :: Iugu.Request.post_response()
+  @doc """
+  Send invoice via email - [Iugu Reference](https://dev.iugu.com/v1.0/reference#enviar-por-email)
+
+  ## Parameters
+
+    - id: Iugu invoice ID
+  """
+  def send_email(id) do
+    %Iugu.Request{path: "#{@resource}/#{id}/send_email"}
+    |> Iugu.Request.post(__MODULE__)
+  end
 end
